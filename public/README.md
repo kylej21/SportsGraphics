1ba885ebbb18891ef023b9749359e7bcf9de9036
kylej21 <kylecj21@gmail.com>
2025-05-12 23:16:29 -0700

    Ground textured

--- public/README.md
+++ public/README.md
@@ -7,3 +7,8 @@ dark greens texture: https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.shutt
 
 bush 3d model: https://free3d.com/3d-model/hazelnut-bush-290678.html
 
+stone texture:
+
+sunset skyline texture: https://pngtree.com/element/down?id=NjY5MDUxNA==&type=1&time=1747114771&token=MzUyYTIwNGQ3ZGM4ZGQ1ZTAxMGY0YTEwM2NlYTI2YzM=&t=0
+
+grass new texture: https://www.freepik.com/free-photo/grass-texture-background_4115918.htm#fromView=keyword&page=1&position=0&uuid=22f4fbb9-e444-4d9e-a7c6-c3e88f1f3803&query=Noise+Texture+Grass
--- src/models/Ground.js
+++ src/models/Ground.js
@@ -1,8 +1,14 @@
 import * as THREE from 'three';
 
+const loader = new THREE.TextureLoader();
+const grassTexture = loader.load('/grassnew.jpg');
+grassTexture.wrapS = THREE.RepeatWrapping;
+grassTexture.wrapT = THREE.RepeatWrapping;
+grassTexture.repeat.set(2, 2); 
+
 export default function Ground(position) {
   const geometry = new THREE.BoxGeometry(1, 0.05, 1);
-  const material = new THREE.MeshStandardMaterial({ color: 0x90ee90 }); 
+  const material = new THREE.MeshStandardMaterial({ map: grassTexture });
 
   const mesh = new THREE.Mesh(geometry, material);
   mesh.position.copy(position);
